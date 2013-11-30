bd8c9b0016f82807721cfeb0af3cb1e1616f9856
max <bmx007@gmail.com>
2013-11-30 10:10:43 +0000

    Add initial files

---
+++ tmp/faillog.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Login attempts info.
+*/
+$login_faillog = array (
+  0 => 
+  array (
+    '10.0.2.2' => 0,
+    'last' => '',
+  ),
+  1 => 
+  array (
+    '10.0.2.2' => 3,
+    'last' => 1385762732,
+  ),
+);
